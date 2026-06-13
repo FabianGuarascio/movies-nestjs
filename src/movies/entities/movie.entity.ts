3dc98a9b89685524ef7167fcfb712a6274beeef1
Fabian Guarascio <guarascio.fabiand@gmail.com>
2026-06-13 00:11:15 -0300

    added entity

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,19 @@
-export class Movie {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ default: 'unknown' })
+  title: string = 'unknown';
+
+  @Column()
+  genre!: string;
+
+  @Column({ nullable: true })
+  year?: number;
+
+  @Column('decimal', { precision: 3, scale: 1 })
+  rating!: number;
+}
