52fd5a0bfd6b31f19b4f756d14b245f4738c6670
Fabian Guarascio <guarascio.fabiand@gmail.com>
2026-06-13 00:51:45 -0300

    added the typeorm into to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { Movie } from './movies/entities/movie.entity';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'movies.db',
+      entities: [Movie],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
