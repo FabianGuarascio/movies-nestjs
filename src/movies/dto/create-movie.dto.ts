1858deb38f7a9df13c25de8cca34f2a5d45a39eb
Fabian Guarascio <guarascio.fabiand@gmail.com>
2026-06-13 01:34:31 -0300

    added validators

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,6 +1,31 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
 export class CreateMovieDto {
+  @IsOptional()
+  @IsString()
   title?: string;
+
+  @IsNotEmpty()
+  @IsString()
   genre!: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1888)
+  @Max(2100)
   year?: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(10)
   rating!: number;
 }
